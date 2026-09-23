ed5f5ee8abd4d61a0c11af5e12ac93915a5fa7ed
Ваше Имя <frestor_99@mail.ru>
2026-09-23 14:53:26 +0300

    lab1

--- src/lab01/ex03.py
+++ src/lab01/ex03.py
@@ -1,9 +1,6 @@
 price = int(input("price: "))
 discount = int(input("discount: "))
 vat = int(input("vat: "))
-#base = price * (1 - discount/100)
-#vat_amount = base * (vat/100)
-#total = base + vat_amount
-print(f"База после скидки: {price * (1 - discount/100)}0 ₽")
-print(f"НДС: {(price * (1 - discount/100))* (vat/100)}0 ₽")
-print(f"Итого к оплате: {(price * (1 - discount/100))+(price * (1 - discount/100))* (vat/100)}0 ₽")
+print(f"База после скидки: {price * (1 - discount/100):.2f} ₽")
+print(f"НДС: {(price * (1 - discount/100)) * (vat/100):.2f} ₽")
+print(f"Итого к оплате: {(price * (1 - discount/100)) + (price * (1 - discount/100)) * (vat/100):.2f} ₽")
